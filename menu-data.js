d97b5d3d17ca5728f1850f109cdb79fa7d1971a8
wedg3 <lindgren@gmail.com>
2026-03-29 21:16:29 +0200

    Add files via upload

---
+++ menu-data.js
@@ -0,0 +1,247 @@
+window.PrickarMenuData = {
+  title: '· PRICKAR · ',
+  categories: [
+    {
+      id: 'matte',
+      title: '· Matte ·',
+      subtitle: '',
+      color: 'matte',
+      subcategories: [
+        {
+          id: 'algebra',
+          title: 'Algebra',
+          subtitle: '',
+          games: [
+            {
+              id: 'allalgebra',
+              title: 'Samlad Algebra',
+              url: 'https://wedg3.github.io/mathgames/',
+              description: 'Flera Algebraspel för enklare länkdelning.',
+              new: 0
+            },
+
+            {
+              id: 'ekvationstraning',
+              title: 'Träna ekvationer',
+              url: 'https://wedg3.github.io/mathgames/equation1.html',
+              description: 'Lös ekvationer genom att göra samma sak på båda sidor.',
+              new: 0
+            },
+            {
+              id: 'skapa-raka-linjer',
+              title: 'Skapa raka linjer',
+              url: 'https://wedg3.github.io/mathgames/slope.html',
+              description: 'Få en känsla för räta linjens ekvation y = kx + m.',
+              new: 0
+            },
+            {
+              id: 'hitta-lutningen',
+              title: 'Hitta lutningen!',
+              url: 'https://wedg3.github.io/mathgames/slopegame.html',
+              description: 'Träna på att hitta linjens ekvation. Försök nå en streak på 10.',
+              new: 0
+            },
+            {
+              id: 'slope-art',
+              title: 'Rita med räta linjen',
+              url: 'https://wedg3.github.io/mathgames/slopeart.html',
+              description: 'Skapa bilder med hjälp av räta linjer och färgläggning.',
+              new: 0
+            }
+          ]
+        },
+        {
+          id: 'aritmetik',
+          title: 'Tal',
+          subtitle: '',
+          games: [
+            {
+              id: 'positionssystemet-1',
+              title: 'Träna * och / med 10ⁿ',
+              url: 'https://wedg3.github.io/mathgames/position1.html',
+              description: 'Träna på att multiplicera och dividera med 10^x.',
+              new: 1
+            }
+          ]
+        }
+      ]
+    },
+    {
+      id: 'no',
+      title: '· NO ·',
+      subtitle: '',
+      color: 'no',
+      subcategories: [
+        {
+          id: 'fysik',
+          title: 'Fysik',
+          subtitle: '',
+		  color: 'matte',
+          games: [
+            {
+              id: 'gravity-sim',
+              title: 'Gravity Sim',
+              url: 'https://wedg3.github.io/gravitysim/',
+              description: 'Utforska gravitation och omloppsbanor. Försök skapa ett stabilt system.',
+              new: 0
+            }
+          ]
+        },
+        {
+          id: 'biologi',
+          title: 'Biologi',
+          subtitle: '',
+		  color: 'no',
+          games: [
+            {
+              id: 'larv-labb',
+              title: 'Larv-labb',
+              url: 'https://wedg3.github.io/larvlabb/',
+              description: 'Skapa biologisk mångfald, testa hypoteser och experimentera i ett virtuellt laboratorium.',
+              new: 0
+            }
+          ]
+        }
+      ]
+    },
+    {
+      id: 'so',
+      title: '· SO ·',
+      subtitle: '',
+      color: 'so',
+      subcategories: [
+        {
+          id: 'geografi',
+          title: 'Geografi',
+          subtitle: '',
+		  color: 'no',
+          games: [
+            {
+              id: 'kartspel',
+              title: 'Kartspel',
+              url: 'https://wedg3.github.io/mapgame/',
+              description: 'Hitta länder, städer och andra platser i ett kartspel.',
+              new: 0
+            }
+          ]
+        },
+		
+		{
+          id: 'historia',
+          title: 'Historia',
+          subtitle: '',
+          games: [
+            {
+              id: 'tiden',
+              title: 'Tiden',
+              url: 'https://wedg3.github.io/time/',
+              description: 'Visualisera tid med tidslinjer utifrån olika teman.',
+              new: 1
+            }
+          ]
+        }
+      ]
+    },
+    {
+      id: 'sprak',
+      title: '· Språk ·',
+      subtitle: '',
+      color: 'sprak',
+      subcategories: [
+        {
+          id: 'engelska',
+          title: 'Engelska',
+          subtitle: '',
+          games: [
+            {
+              id: 'which-one',
+              title: 'Which One?',
+              url: 'https://wedg3.github.io/whichone/',
+              description: 'Spela en version av Vem där? med ja/nej-frågor på två datorer.',
+              new: 0
+            },
+            {
+              id: 'verb-volcano',
+              title: 'Verb Volcano',
+              url: 'https://wedg3.github.io/verbvolcano/',
+              description: 'Träna engelska oregelbundna verb.',
+              new: 0
+            },
+            {
+              id: 'word-volcano',
+              title: 'Word Volcano',
+              url: 'https://wedg3.github.io/wordvolcano/',
+              description: 'Skriv de engelska orden innan de når lavan.',
+              new: 0
+            },
+            {
+              id: 'word-volcano-lists',
+              title: 'Word Volcano listor',
+              url: 'https://wedg3.github.io/wordvolcano/lists.html',
+              description: 'Samlingar med ord för Word Volcano.',
+              new: 0
+            }
+          ]
+        }
+      ]
+    },
+    {
+      id: 'idh',
+      title: '· Idrott ·',
+      subtitle: '',
+      color: 'idh',
+      subcategories: [
+        {
+          id: 'orientering',
+          title: 'Orientering',
+          subtitle: '',
+          games: [
+            {
+              id: 'storm-survival',
+              title: 'Storm Survival',
+              url: 'https://wedg3.github.io/argames/stormsurvivalinfo.html',
+              description: 'Överlevnadslek med orientering som fungerar ute i skogen eller stan.',
+              new: 0
+            },
+            {
+              id: 'storm-seek',
+              title: 'Storm Seek',
+              url: 'https://wedg3.github.io/argames/stormseekinfo.html',
+              description: 'Kurragömma möter orientering.',
+              new: 0
+            },
+            {
+              id: 'storm-tag',
+              title: 'Storm Tag',
+              url: 'https://wedg3.github.io/argames/stormtaginfo.html',
+              description: 'Kull möter orientering med lite mer action.',
+              new: 0
+            }
+          ]
+        }
+      ]
+    },
+    {
+      id: 'blandat',
+      title: '· Annat ·',
+      subtitle: '',
+      color: 'blandat',
+      subcategories: [
+        {
+          id: 'studieverktyg',
+          title: 'Studieverktyg',
+          subtitle: '',
+          games: [
+            {
+              id: 'flashcards',
+              title: 'Flashcards',
+              url: 'https://wedg3.github.io/flashcards/lists.html',
+              description: 'Plugga inför prov med färdiga flashcardsamlingar.',
+              new: 0
+            }
+          ]
+        }
+      ]
+    }
+  ]
+};
